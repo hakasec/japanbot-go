43a1b22df4ccd69ac2df855df9225fea35fdf430
hakasec <soperdeclan@gmail.com>
2018-08-17 01:19:01 +0100

    Add basic database groundwork

---
+++ bot/database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+
+	// Driver for sqlite3
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/hakasec/japanbot-go/bot/config"
+)
+
+// DBConnection is an extension of sql.DB
+type DBConnection struct {
+	*sql.DB
+}
+
+// OpenFromConfig creates a DBConnection from a given DBConfiguration
+func OpenFromConfig(config config.DBConfiguration) (*DBConnection, error) {
+	db, err := sql.Open(config.DriverName, config.ConnString)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DBConnection{DB: db}, nil
+}
